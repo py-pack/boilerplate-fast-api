6f9792f5f06ba8eac0f6871b79e2bd1c7dff7a53
PyPack <serg.karetnuk333@gmail.com>
2024-07-28 18:26:42 +0200

    add db_helper + autoload config env

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 ## Install Packeges
 
-poetry add fastapi "uvicorn[standart]" "pydantic[email]" pydantic-settings
-poetry add "sqlalchemy[asyncio]"
+    poetry add fastapi "uvicorn[standart]" "pydantic[email]" pydantic-settings
 
+    poetry add "sqlalchemy[asyncio]"
+
+## Links
+
+ - [SQL Alchemy Engine](https://docs.sqlalchemy.org/en/20/core/engines.html)
+
+ - [Pydantic Setting](https://docs.pydantic.dev/latest/concepts/pydantic_settings/#case-sensitivity)
--- fastapi-app/core/config.py
+++ fastapi-app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -11,11 +12,32 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 5432
+    database: str = ''
+    user: str = ''
+    password: str = ''
+    echo: bool = False,  # Виводити логи DB
+    echo_pool: bool = False,  # Виводити більш широкі логи
+    pool_size: int = 5,  # кількість відкрити з'єднань
+    max_overflow: int = 10,  # кількість підключень в пулі
+
+    @property
+    def url(self) -> PostgresDsn:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),     # порядо підвантаження мержа змінинних
+        case_sensitive=False,                   # не важливий регістр
+        env_nested_delimiter="__",              # розділювач
+        env_prefix="APP_CONFIG__",              # префікс змінних, які будуть автоматично парситись
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-
-    db_uri: str = ""
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
